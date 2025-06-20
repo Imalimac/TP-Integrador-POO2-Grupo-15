813fd563840c22cacce4e2501a0b4ec675c04a29
Tomas Dominguez <tomaselias.dw@gmail.com>
2025-06-20 15:06:06 -0300

    clase abstracta Organizacion

--- src/main/java/unq/cazaDeVinchucas/modelo/Organizacion.java
+++ src/main/java/unq/cazaDeVinchucas/modelo/Organizacion.java
@@ -1,5 +1,16 @@
 package main.java.unq.cazaDeVinchucas.modelo;
 
 public abstract class Organizacion {
+	private List FuncionalidadExterna FuncionalidadExterna
+	
+		public abstract void subirNuevaMuestra(ZonaDeCobertura areaCobertura, Muestra muestra);
+
+		public abstract  void validarMuestra(ZonaDeCobertura areaCobertura, Muestra muestra);
+		public abstract void registrarseA(ZonaDeCobertura zona);
+		public abtsract void desregistrarseDe(ZonaDeCobertura zona);
+		
+	}
+	
 
 }
+ 
